d23c0cbb4733dcc70777f9ea695125b21e38ae63
Joe Boulos <jab12288@gmail.com>
2019-01-08 11:30:57 -0800

    setting up proxy, in progress

--- Schedule/server/index.js
+++ Schedule/server/index.js
@@ -7,22 +7,22 @@ const app = express();
 
 app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // Schedule endpoint
 app.get('/espn/schedules', (req, res) => {
   ScheduleDB.find({}, (err, data) => {
   })
-  .limit(17)
-  .sort({week: 1})
-  .then(function(data) {
-    res.send(data);
-  })
-  .catch(function(err) {
-    console.err(err);
-  })
-})
+    .limit(17)
+    .sort({ week: 1 })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.err(err);
+    });
+});
 
 
 // Feed endpoint
@@ -30,15 +30,15 @@ app.get('/espn/feeds', (req, res) => {
   FeedDB.find({}, (err, data) => {
   })
   // 3 entries per game
-  .limit(51)
-  .sort({timestamp: 1})
-  .then(function(data) {
-    res.send(data);
-  })
-  .catch(function(err) {
-    console.err(err);
-  })
-})
+    .limit(51)
+    .sort({ timestamp: 1 })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.err(err);
+    });
+});
 
 
 const port = process.env.PORT || 3000;
@@ -46,4 +46,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
-
