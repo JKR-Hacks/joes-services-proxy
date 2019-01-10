748f97a542733edb2767ba8e6ea776c0a3510a4e
Joe Boulos <jab12288@gmail.com>
2019-01-09 16:28:18 -0800

    most recent

--- Schedule/client/src/App.js
+++ Schedule/client/src/App.js
@@ -2,50 +2,18 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import Schedule from './components/Schedule';
-// import Feed from './components/Feed.js';
-// import SidebarSchedule from './components/SidebarSchedule.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       schedule: [],
-      // ramsFeed: [],
-      // home view should render schedule sidebar and gamefeed on the same page
-      // eslint-disable-next-line max-len
-      // for now, test components by changing the "view" property until changeView method is implemented
       view: 'schedule',
     };
   }
 
-  // componentDidMount() {
-  //   // fetch schedule data
-  //   fetch('espn/schedules')
-  //     .then(res => res.json())
-  //     .then((data) => {
-  //       this.setState({
-  //         schedule: data,
-  //       });
-  //     })
-  //     .catch((err) => {
-  //       console.log('error: ', err);
-  //     });
-
-  // fetch feed data
-  // fetch('espn/feeds')
-  //   .then(res => res.json())
-  //   .then((data) => {
-  //     this.setState({
-  //       ramsFeed: data,
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     console.log('error: ', err);
-  //   });
-  // }
-
   componentDidMount() {
-    axios.get('espn/schedules', {
+    axios.get('http://localhost:3001/espn/schedules', {
       method: 'GET',
       mode: 'no-cors',
       headers: {
@@ -57,7 +25,6 @@ class App extends Component {
         // eslint-disable-next-line prefer-destructuring
         const data = res.data;
         this.setState({
-          // eslint-disable-next-line react/no-unused-state
           schedule: data,
         });
       })
@@ -66,30 +33,9 @@ class App extends Component {
       });
   }
 
-  // TODO
-  // implement changeView method to switch from main page to schedule page
-
   // eslint-disable-next-line consistent-return
   renderView() {
-    // receives props object, destructure properties
-    // const { schedule, ramsFeed, view } = this.state;
     const { schedule, view } = this.state;
-    // if (view === 'home') {
-    //   return (
-    //     <div id="home">
-    //       <div id="sidebarSchedule">
-    //         <SidebarSchedule
-    //           ramsSchedule={schedule}
-    //         />
-    //       </div>
-    //       <div id="feed">
-    //         <Feed
-    //           ramsFeed={ramsFeed}
-    //         />
-    //       </div>
-    //     </div>
-    //   );
-    // }
     if (view === 'schedule') {
       return (
         <div id="fullschedule">
@@ -99,24 +45,6 @@ class App extends Component {
         </div>
       );
     }
-    // if (view === 'feed') {
-    //   return (
-    //     <div id="feed">
-    //       <Feed
-    //         ramsFeed={ramsFeed}
-    //       />
-    //     </div>
-    //   );
-    // }
-    // if (view === 'sidebarSchedule') {
-    //   return (
-    //     <div id="sidebarSchedule">
-    //       <SidebarSchedule
-    //         ramsSchedule={schedule}
-    //       />
-    //     </div>
-    //   );
-    // }
   }
 
   render() {
--- Schedule/server/index.js
+++ Schedule/server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const ScheduleDB = require('../database/Models/ScheduleDB.js');
-// const FeedDB = require('../database/Models/FeedDB.js');
 
 const app = express();
 
+app.use((cors));
 app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,23 +24,6 @@ app.get('/espn/schedules', (req, res) => {
       console.err(err);
     });
 });
-
-
-// Feed endpoint
-// app.get('/espn/feeds', (req, res) => {
-//   FeedDB.find({}, (err, data) => {
-//   })
-//   // 3 entries per game
-//     .limit(51)
-//     .sort({ timestamp: 1 })
-//     .then((data) => {
-//       res.send(data);
-//     })
-//     .catch((err) => {
-//       console.err(err);
-//     });
-// });
-
 
 const port = process.env.PORT || 3001;
 
