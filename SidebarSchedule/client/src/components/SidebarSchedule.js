b075c2f1a9a5720e45d50ed58263e69c0431bf77
Joe Boulos <jab12288@gmail.com>
2019-01-12 14:25:28 -0800

    updated proxy components, all services render on proxy server, styling has been updated

--- SidebarSchedule/client/src/components/SidebarSchedule.js
+++ SidebarSchedule/client/src/components/SidebarSchedule.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 
 const style = {
@@ -9,56 +11,35 @@ const style = {
 
 
 const SidebarSchedule = ({ ramsSchedule }) => (
-  <div className="container-fluid">
+  <div className="col-sm">
+    <p className="h5 text-dark font-weight-bold">REGULAR SEASON</p>
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col" />
+          <th scope="col" />
+          <th scope="col" />
+          <th scope="col" />
+        </tr>
+      </thead>
 
-    <div className="pos-f-t">
-      <div className="collapse" id="navbarToggleExternalContent">
-        <div className="bg-dark p-4">
-          <h4 className="text-white">Collapsed content</h4>
-          <span className="text-muted">Toggleable via the navbar brand.</span>
-        </div>
-      </div>
-      <nav className="navbar navbar-dark bg-dark">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-      </nav>
-    </div>
+      <tbody>
 
 
-    <div className="row justify-content-start">
-      <div className="col-4">
-        <h4>Regular Season</h4>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col" />
-              <th scope="col" />
-              <th scope="col" />
-              <th scope="col" />
-            </tr>
-          </thead>
+        {ramsSchedule.map(schedule => (
 
-          <tbody>
-
-
-            {ramsSchedule.map(schedule => (
-
-              <tr key={schedule.id}>
-                <td>
-                  <img src={`${schedule.opponentlogo}`} className="rounded-circle" width={20} height={20} style={style.image} />
-                  {schedule.vs}
-                </td>
-                <td>{schedule.opponent}</td>
-                <td>{schedule.wl}</td>
-                <td><p>{schedule.result}</p></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-
+          <tr key={schedule.id}>
+            <td>
+              <img src={`${schedule.opponentlogo}`} className="rounded-circle" width={20} height={20} style={style.image} />
+              {schedule.vs}
+            </td>
+            <td>{schedule.opponent}</td>
+            <td>{schedule.wl}</td>
+            <td><p>{schedule.result}</p></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </div>
 );
 
