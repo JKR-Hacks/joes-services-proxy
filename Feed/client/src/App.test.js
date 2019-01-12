6747033016d32e40d6aa7c192cd9b1d669ecf288
Joe Boulos <jab12288@gmail.com>
2019-01-12 11:52:12 -0800

    updated tests for Feed, Schedule, ScheduleSidebar components

--- Feed/client/src/App.test.js
+++ Feed/client/src/App.test.js
@@ -1,10 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable no-undef */
-import App from './App';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import axios from 'axios';
+import App from './App';
 
-jest.mock('./components/Schedule', () => 'schedule');
+jest.mock('./components/Feed', () => 'Feed');
 
 describe('App component', () => {
   it('should render App component', () => {
@@ -19,35 +20,24 @@ describe('App component', () => {
   });
 });
 
-describe('Schedule component', () => {
-  it('should render Schedule component', () => {
-    const tree = mount(<App />)
-    expect(toJson(tree)).toMatchSnapshot()
-  })
+describe('Feed component', () => {
+  it('should render Feed component', () => {
+    const tree = mount(<App />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
 });
 
-describe("API calls to database", () => {
+describe('API calls to database', () => {
   it('Should call componentDidMount', () => {
     const spy = jest.spyOn(App.prototype, 'componentDidMount');
     const wrapper = mount(<App />);
     wrapper.instance().componentDidMount();
     expect(spy).toHaveBeenCalled();
   });
-  it('Should fetch /espn/schedules', () => {
-    const spy = jest.spyOn(App.prototype, 'componentDidMount');
-    const wrapper = mount(<App />);
-    wrapper.instance().componentDidMount(fetch('espn/schedules'));
-    expect(spy).toHaveBeenCalled();
-  });
   it('Should fetch /espn/feeds', () => {
     const spy = jest.spyOn(App.prototype, 'componentDidMount');
     const wrapper = mount(<App />);
-    wrapper.instance().componentDidMount(fetch('espn/feeds'));
+    wrapper.instance().componentDidMount(axios.get('espn/feeds'));
     expect(spy).toHaveBeenCalled();
   });
-})
-
-// it("should render Schedule component", () => {
-//   const tree = renderer.create(<Schedule/>).toJSON()
-//   expect(tree).toMatchSnapshot();
-// })
+});
