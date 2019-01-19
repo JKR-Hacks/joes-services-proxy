d0eff487e6170cbcd37576a56b645cfd6f0f0f34
Joe Boulos <jab12288@gmail.com>
2019-01-19 12:50:25 -0800

    updated proxy sidebar

--- SidebarSchedule/database/Models/ScheduleDB.js
+++ SidebarSchedule/database/Models/ScheduleDB.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-// const onlineDb = require('../../../config/keys');
-const onlineDb = require('../../../sidebarSchedule/config/keys');
+const onlineDb = require('../../config/keys');
 
 mongoose.connect(onlineDb.mongoURI, { useNewUrlParser: true });
 
